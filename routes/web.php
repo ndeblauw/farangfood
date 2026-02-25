10294c3f80e2469fc63f4b9cc74f6de0d3ba33fe
Nico Deblauwe <nico@bluepundit.eu>
2026-02-25 10:58:20 +0700

    CONTENT :pencil: Add some structure to routes file

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 
+/*
+ * Public routes
+ */
+
 Route::get('/', \App\Http\Controllers\WelcomeController::class)->name('home');
 
+
+/*
+ * User zone routes
+ */
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
 require __DIR__.'/settings.php';
+
+
+/*
+ * Admin zone routes
+ */
