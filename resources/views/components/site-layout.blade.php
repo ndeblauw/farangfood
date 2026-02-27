5229ce1eed42f14d9e6a048dc4928ff5a496a312
Nico Deblauwe <nico@bluepundit.eu>
2026-02-27 11:38:06 +0700

    DESIGN :art: Welcome page using sitelayout

--- resources/views/components/site-layout.blade.php
+++ resources/views/components/site-layout.blade.php
@@ -1,9 +1,17 @@
-<html>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <title>List of Shops</title>
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+
 </head>
-<body class="bg-sky-100">
+<body class="bg-sky-100" style="font-family: 'Instrument Sans', sans-serif;">
 
     <x-site-navigation/>
 
