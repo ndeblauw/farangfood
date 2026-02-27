a6c586e5f8cc4c08e2cb032a4c8822a3c78669df
Nico Deblauwe <nico@bluepundit.eu>
2026-02-27 11:17:39 +0700

    DESIGN :art: Add some styling to our shops pages

--- resources/views/shops/index.blade.php
+++ resources/views/shops/index.blade.php
@@ -1,8 +1,22 @@
 <html>
 <head>
     <title>List of Shops</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+
 </head>
-<body>
+<body class="bg-purple-200">
+
+<div class="bg-purple-900 text-purple-50">
+
+    <ul class="flex space-x-4 p-4">
+        <li><a href="{{route('home')}}">Home</a></li>
+        <li><a href="{{route('shops.index')}}">Shops</a></li>
+    </ul>
+
+</div>
+
+<div class="p-4">
+
 
     <h1>List of Shops</h1>
     <ul>
@@ -10,6 +24,7 @@
             <li> <a href="{{ route('shops.show', $shop->id) }}">{{ $shop->name }}</a></li>
         @endforeach
     </ul>
+</div>
 </body>
 </html>
 
--- resources/views/shops/show.blade.php
+++ resources/views/shops/show.blade.php
@@ -1,35 +1,49 @@
 <html>
 <head>
     <title>Details of {{$shop->name}}</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
-<body>
+<body class="bg-purple-200">
 
-<h1>Details of {{$shop->name}}</h1>
-<p><em>{{$shop->address}}</em></p>
-<p>{{$shop->description}}</p>
+<div class="bg-purple-900 text-purple-50">
 
-<hr/>
-<h2>Foods present</h2>
-<ul>
-    @foreach($shop->foods as $food)
-        <li>{{$food->name}}</li>
-    @endforeach
-</ul>
+    <ul class="flex space-x-4 p-4">
+        <li><a href="{{route('home')}}">Home</a></li>
+        <li><a href="{{route('shops.index')}}">Shops</a></li>
+    </ul>
 
-<hr/>
-<h2>Reviews</h2>
-<ul>
-    @foreach($shop->reviews as $review)
-        <li>
-            <p>
-                <strong>{{$review->author->name}}:</strong>
-                {{$review->comment}}
-            </p>
-        </li>
-    @endforeach
-</ul>
+</div>
 
-<a href="{{ route('shops.index') }}">Back to list of shops</a>
+<div class="p-4">
+
+    <h1 class="text-2xl font-bold">Details of {{$shop->name}}</h1>
+    <p><em>{{$shop->address}}</em></p>
+    <p>{{$shop->description}}</p>
+
+    <div class="mt-6 mb-2">
+        <h2 class="inline text-xl font-bold text-purple-50 bg-purple-700">Foods present</h2>
+        <ul class="list-disc list-inside">
+            @foreach($shop->foods as $food)
+                <li>{{$food->name}}</li>
+            @endforeach
+        </ul>
+
+    </div>
+
+    <div class="mt-6 mb-2">
+        <h2 class="inline text-xl font-bold text-purple-50 bg-purple-700">Reviews</h2>
+        <ul class="list-disc list-inside">
+            @foreach($shop->reviews as $review)
+                <li>
+                    <strong>{{$review->author->name}}:</strong>
+                    {{$review->comment}}
+                </li>
+            @endforeach
+        </ul>
+    </div>
+
+    <a href="{{ route('shops.index') }}">Back to list of shops</a>
+</div>
 </body>
 </html>
 
