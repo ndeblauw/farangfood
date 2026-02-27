c1ef5996d1571fd8168aa4e71d88f21909398145
Nico Deblauwe <nico@bluepundit.eu>
2026-02-27 11:45:17 +0700

    BUGFIX :bug: Remove vite and replace by cdn for styling auth pages !!!

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -10,5 +10,5 @@
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
-@vite(['resources/css/app.css', 'resources/js/app.js'])
+<script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 @fluxAppearance
